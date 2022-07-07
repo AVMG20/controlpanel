cd9e0793dad90c7bc3cca0fd62b5ce3fdd1ea66b
1Day <ownerdennis8@gmail.com>
2022-07-07 13:31:07 +0200

    Nullable Descriptions

--- database/migrations/2022_02_23_173837_create_nodes_table.php
+++ database/migrations/2022_02_23_173837_create_nodes_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('location_id')->references('id')->on('locations')->cascadeOnDelete();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
