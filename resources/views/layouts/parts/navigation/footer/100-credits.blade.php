73dcd37a5f6e169a591834f087576d617d1c724d
1day2die <ownerdennis8@gmail.com>
2022-09-07 23:40:29 +0200

    FooterNavigation

---
+++ resources/views/layouts/parts/navigation/footer/100-credits.blade.php
@@ -0,0 +1,6 @@
+<!-- List -->
+<ul class="list-inline list-group-flush list-group-borderless text-md-end mb-0">
+    <li class="list-inline-item px-0 px-sm-2">
+        <i class="fa-brands fa-discord"></i> <a href="https://discord.com/invite/4Y6HjD2uyU">Powered by Controlpanel.gg</a>
+    </li>
+</ul>
