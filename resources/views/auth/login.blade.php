e3da4e9b23926bfcce2865d2ca24e19734fc79a9
1day2die <ownerdennis8@gmail.com>
2022-10-25 21:54:04 +0200

    show file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,8 @@
                         <h1 class="mb-3 h3">{{ __('Welcome back') }}</h1>
                     </div>
 
-                    @if($settings->logo_enabled && file_exists(public_path("images/logo.png")))
-                        <img src="{{asset("images/logo.png")}}">
+                    @if($settings->logo_enabled && file_exists(storage_path("app/public/images/logo.png")))
+                        <img src="{{asset("storage/images/logo.png")}}">
                     @endif
 
                     <form class="mt-4" action="{{ route('login') }}" method="POST">
